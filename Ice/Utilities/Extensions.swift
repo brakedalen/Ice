bf9a3b4b1805dce3b6432391d15794fc04f2e8b0
offyotto <245704867+offyotto@users.noreply.github.com>
2026-07-22 13:46:06 -0400

    Handle fully transparent average colors

--- Ice/Utilities/Extensions.swift
+++ Ice/Utilities/Extensions.swift
@@ -160,6 +160,12 @@ extension CGImage {
             }
         }
 
+        // A fully transparent image has no pixels above the alpha
+        // threshold, and therefore no average color.
+        guard count > 0 else {
+            return nil
+        }
+
         // Components are currently in integer format (0 to 255), but need
         // to be converted to floating point (0 to 1). Makes more sense to
         // scale the count up to match the components, rather than scale
